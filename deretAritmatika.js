b79bd57cf55de8ff988511449b8a7e398cd91091
bilal <mbilalghz@gmail.com>
2019-02-06 15:04:19 +0700

    deret artimatika

---
+++ deretAritmatika.js
@@ -0,0 +1,25 @@
+function tentukanDeretAritmatika(arr) {
+    // you can only write your code here!
+    var banding = arr[1] - arr[0]
+    console.log(banding)
+    var flag = false
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i+1] !== undefined) {
+        if (arr[i+1] - arr[i] === banding ) {
+            flag = true
+        }else {
+          return false
+        }
+      }
+    }
+    if (flag === true) {
+      return true
+    }
+  }
+  
+  // TEST CASES
+  console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
+  console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
+  console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+  console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+  console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
