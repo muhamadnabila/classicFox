cde529bafa66178f5a32b30a879bad14fae042ce
MuhamadNabila <44949192+muhamadnabila@users.noreply.github.com>
2019-02-07 13:42:41 +0700

    Update angkaPrima.js
    Update

--- angkaPrima.js
+++ angkaPrima.js
@@ -1,18 +1,22 @@
 function angkaPrima(angka) {
-    // you can only write your code here!
-    for (var i = 2; i < angka; i++) {
-      console.log(i)
-      if (angka % i === 0) {
-        return false
-      }else {
-        return true
-      }
+  // you can only write your code here!
+  var flag = true
+  for (var i = 2; i < angka; i++) {
+    // console.log(i)
+    if (angka % i === 0) {
+      flag = false
     }
   }
-  
-  // TEST CASES
-  console.log(angkaPrima(3)); // true
-  console.log(angkaPrima(7)); // true
-  console.log(angkaPrima(6)); // false
-  console.log(angkaPrima(23)); // true
-  console.log(angkaPrima(33)); // true
+  if (flag === true) {
+      return true
+  } else {
+      return false
+  }
+}
+
+// TEST CASES
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
