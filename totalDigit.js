98bc37cca6c940fd6c120d72ea6845cb5b7a538b
bilal <mbilalghz@gmail.com>
2019-02-08 14:22:28 +0700

    total digit (rekursif)

---
+++ totalDigit.js
@@ -0,0 +1,17 @@
+function totalDigitRekursif(angka) {
+    // you can only write your code here!
+    var stringAngka = String(angka)
+    if (stringAngka.length === 0) {
+        return 0
+    }else {
+        var counter = Number(stringAngka[0])
+        return counter + totalDigitRekursif(stringAngka.slice(1))
+    }
+  }
+  
+  // TEST CASES
+  console.log(totalDigitRekursif(512)); // 8
+  console.log(totalDigitRekursif(1542)); // 12
+  console.log(totalDigitRekursif(5)); // 5
+  console.log(totalDigitRekursif(21)); // 3
+  console.log(totalDigitRekursif(11111)); // 5
