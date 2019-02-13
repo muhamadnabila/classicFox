cb27259f265d9735e1aa246d0af4425fe0d1a607
bilal <mbilalghz@gmail.com>
2019-02-13 13:59:55 +0700

    update tukar besar kecil

---
+++ exercise/tukarBesarKecil.js
@@ -0,0 +1,41 @@
+function tukarBesarKecil(kalimat) {
+    // you can only write your code here!
+    //--------------recursive-----------------------
+    // if (kalimat.length === 0) {
+    //   return ''
+    // }else {
+    //   var result = ''
+    //   var hurufKecil = kalimat.toLowerCase()
+      // var hurufBesar = kalimat.toUpperCase()
+    //   for (var i = 0; i < hurufBesar.length; i++) {
+    //     if (kalimat[0] === hurufBesar[i]) {
+    //         result += hurufKecil[i]
+    //     } 
+    //     else if (kalimat[0] === hurufKecil[i]) {
+    //       result += hurufBesar[i]
+    //     }
+    //   return result + tukarBesarKecil(kalimat.slice(1))
+    //   }
+    // }
+    //-------------------cara biasa---------------------
+    var result = ''
+    var checkLowerCase = kalimat.toLowerCase()
+    var checkUpperCase = kalimat.toUpperCase() 
+    
+    for (var i = 0; i < kalimat.length; i++) {
+      if (kalimat[i] === checkLowerCase[i]) {
+        result += kalimat[i].toUpperCase()
+      }
+      else if (kalimat[i] === checkUpperCase[i]) {
+        result += kalimat[i].toLowerCase()
+      }
+    }
+    return result
+  }
+  
+  // TEST CASES
+  console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+  console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+  console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+  console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+  console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
